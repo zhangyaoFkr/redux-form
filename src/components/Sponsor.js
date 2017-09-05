b049269498b599a40dc9dc2686cc4d8a9e4adaba
Erik Rasmussen <rasmussenerik@gmail.com>
2017-09-05 23:43:41 +0200

    0.0.56

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -5,7 +5,10 @@ export default () => (
     <a
       href="https://app.codesponsor.io/link/P3WjkUw9LiYdiQBdHrtHZSSL/erikras/redux-form"
       rel="nofollow"
-      style={{ background: 'white' }}
+      style={{
+        background: 'rgba(255, 255, 255, 0.9)',
+        display: 'inline-block'
+      }}
     >
       <img
         src="https://app.codesponsor.io/embed/P3WjkUw9LiYdiQBdHrtHZSSL/erikras/redux-form.svg"

85f4e6af7f25e655e4bb54a2c9d6c51d67d1032c
Erik Rasmussen <rasmussenerik@gmail.com>
2017-09-05 23:24:34 +0200

    sponsor formatting 0.0.55

--- src/components/Sponsor.js
+++ src/components/Sponsor.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default () => (
-  <div>
+  <div style={{ textAlign: 'center' }}>
     <a
       href="https://app.codesponsor.io/link/P3WjkUw9LiYdiQBdHrtHZSSL/erikras/redux-form"
       rel="nofollow"
+      style={{ background: 'white' }}
     >
       <img
         src="https://app.codesponsor.io/embed/P3WjkUw9LiYdiQBdHrtHZSSL/erikras/redux-form.svg"

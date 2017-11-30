51e2e0c640c69da1e0865a039d450ec697f864d5
Erik Rasmussen <rasmussenerik@gmail.com>
2017-11-30 17:43:42 +0100

    killed client-side react

--- src/client.js
+++ src/client.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './components/App'
+// import React from 'react'
+// import ReactDOM from 'react-dom'
+// import App from './components/App'
 
-if (typeof window !== 'undefined') {
-  // what a hack!! :-)
-  const __html = document.getElementsByTagName('h1')[0].parentNode.innerHTML
-  window.initReact = props =>
-    ReactDOM.hydrate(
-      <App {...props}>
-        <div dangerouslySetInnerHTML={{ __html }} />
-      </App>,
-      document.getElementById('content')
-    )
-}
+// if (typeof window !== 'undefined') {
+//   // what a hack!! :-)
+//   const __html = document.getElementsByTagName('h1')[0].parentNode.innerHTML
+//   window.initReact = props =>
+//     ReactDOM.hydrate(
+//       <App {...props}>
+//         <div dangerouslySetInnerHTML={{ __html }} />
+//       </App>,
+//       document.getElementById('content')
+//     )
+// }

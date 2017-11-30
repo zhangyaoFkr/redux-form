a5f8875e0904a589287b760a57909649ac7e5388
Erik Rasmussen <rasmussenerik@gmail.com>
2017-11-30 17:53:07 +0100

    added window check

--- src/client.js
+++ src/client.js
@@ -2,18 +2,18 @@
 // import ReactDOM from 'react-dom'
 // import App from './components/App'
 
-// if (typeof window !== 'undefined') {
-//   // what a hack!! :-)
-//   const __html = document.getElementsByTagName('h1')[0].parentNode.innerHTML
-//   window.initReact = props =>
-//     ReactDOM.hydrate(
-//       <App {...props}>
-//         <div dangerouslySetInnerHTML={{ __html }} />
-//       </App>,
-//       document.getElementById('content')
-//     )
-// }
+if (typeof window !== 'undefined') {
+  //   // what a hack!! :-)
+  //   const __html = document.getElementsByTagName('h1')[0].parentNode.innerHTML
+  //   window.initReact = props =>
+  //     ReactDOM.hydrate(
+  //       <App {...props}>
+  //         <div dangerouslySetInnerHTML={{ __html }} />
+  //       </App>,
+  //       document.getElementById('content')
+  //     )
 
-window.initReact = () => {
-  // noop
+  window.initReact = () => {
+    // noop
+  }
 }
